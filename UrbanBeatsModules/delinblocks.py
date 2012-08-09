6605030c9a5cf5f3526f514758c7989da9646848
Clemens Degasper <Clemens.degasper@gmail.com>
2012-08-09 16:50:40 +0200

    added Gui

--- UrbanBeatsModules/delinblocks.py
+++ UrbanBeatsModules/delinblocks.py
@@ -22,11 +22,9 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
-#from delinblocksguic import *
+from delinblocksguic import *
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-#from pyvibe import *
-#import pyvibe
 from pydynamind import *
 import math
 
@@ -1123,4 +1121,7 @@ class delinblocks(Module):
     ########################################################
     #LINK WITH GUI                                         #
     ########################################################        
-   
+    def createInputDialog(self):
+        form = activatedelinblocksGUI(self, QApplication.activeWindow())
+        form.show()
+        return True 
